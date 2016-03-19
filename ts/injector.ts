b237ea7421b79a2943d1da1daa8c09cacd1492c4
igorzg <igor.zg1987@gmail.com>
2016-03-19 22:19:25 +0100

    Removed js from files

--- ts/injector.ts
+++ ts/injector.ts
@@ -3,26 +3,6 @@ export * from 'reflect-metadata';
 const METADATA_KEYS = 'design:paramtypes';
 const COMPONENT_CONFIG_KEYS = 'component:config';
 
-export function stringify(token):string {
-	if (typeof token === 'string') {
-		return token;
-	}
-
-	if (token === undefined || token === null) {
-		return '' + token;
-	}
-
-	if (token.name) {
-		return token.name;
-	}
-	if (token.overriddenName) {
-		return token.overriddenName;
-	}
-
-	var res = token.toString();
-	var newLineIndex = res.indexOf("\n");
-	return (newLineIndex === -1) ? res : res.substring(0, newLineIndex);
-}
 /**
  * @license Mit Licence 2016
  * @since 1.0.0
@@ -86,7 +66,7 @@ export class Injector {
 					return ChildClass;
 				} else if (!this.has(ChildClass)) {
 					throw new Error(` Invalid injection type for
-						${stringify(ChildClass)}
+						${ChildClass.toString()}
 					`);
 				}
 				return this.get(ChildClass);
@@ -120,62 +100,7 @@ export class Injector {
 		if (!Array.isArray(o)) {
 			return new t();
 		}
-		switch (t.length) {
-			case 1:
-				return new t(o[0]);
-			case 2:
-				return new t(o[0], o[1]);
-			case 3:
-				return new t(o[0], o[1], o[2]);
-			case 4:
-				return new t(o[0], o[1], o[2], o[3]);
-			case 5:
-				return new t(o[0], o[1], o[2], o[3], o[4]);
-			case 6:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5]);
-			case 7:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6]);
-			case 8:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7]);
-			case 9:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8]);
-			case 10:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9]);
-			case 11:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10]);
-			case 12:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11]);
-			case 13:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12]);
-			case 14:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13]);
-			case 15:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14]);
-			case 16:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14], o[15]);
-			case 17:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14], o[15], o[16]);
-			case 18:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14], o[15], o[16], o[17]);
-			case 19:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14], o[15], o[16], o[17], o[18]);
-			case 20:
-				return new t(o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7], o[8], o[9], o[10],
-					o[11], o[12], o[13], o[14], o[15], o[16], o[17], o[18], o[19]);
-			default:
-				return new t();
-		}
-
-		throw new Error(
-			`Cannot create a instance for '${stringify(t)}' because its constructor has more than 20 arguments`);
+		return Reflect.construct(t, o);
 	};
 }
 
