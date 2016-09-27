34b0bc0e6e7d88060aa87539b1d47c1d8daccb40
igorzg <igor.zg1987@gmail.com>
2016-09-27 23:37:53 +0200

    Remove default providers on module and controllers size check

--- src/decorators/module.ts
+++ src/decorators/module.ts
@@ -1,5 +1,3 @@
-import {Logger} from "../logger/logger";
-import {Router} from "../router/router";
 import {IModuleMetadata} from "../interfaces/imodule";
 import {isArray, isClass} from "../core";
 import {Metadata} from "../injector/metadata";
@@ -16,10 +14,10 @@ import {Metadata} from "../injector/metadata";
  * Define module in your application
  *
  * @example
- * import {Module, Router} from "node-ee";
+ * import {Module, Router} from "typeix";
  *
  * \@Module({
- *  providers:[Router]
+ *  providers:[Logger, Router]
  * })
  * class Application{
  *    constructor(router: Router) {
@@ -30,19 +28,9 @@ import {Metadata} from "../injector/metadata";
 export var Module = (config: IModuleMetadata) => (Class) => {
   if (!isClass(Class)) {
     throw new TypeError(`@Module is allowed only on class`);
-  } else if (config.controllers.length === 0) {
-    throw new TypeError(`@Module must have defined at least one controller`);
   }
   if (!isArray(config.providers)) {
     config.providers = [];
-  }
-  // add router to default config
-  if (!Metadata.hasProvider(config.providers, Router)) {
-    config.providers.unshift(Router);
-  }
-  // add logger to start of providers
-  if (!Metadata.hasProvider(config.providers, Logger)) {
-    config.providers.unshift(Logger);
   }
   config.providers = config.providers.map(ProviderClass => Metadata.verifyProvider(ProviderClass));
   Metadata.setComponentConfig(Class, config);
--- src/interfaces/imodule.ts
+++ src/interfaces/imodule.ts
@@ -4,13 +4,17 @@ import {RouteRuleConfig} from "./iroute";
  * @since 1.0.0
  * @interface
  * @name IModuleMetadata
+ * @param {String} name
  * @param {Array<RouteRuleConfig>} routes
+ * @param {Array<IModuleMetadata>} modules
+ * @param {Array<IProvider|Function>} controllers
  * @param {Array<IProvider|Function>} providers
  *
  * @description
  * Bootstrap class config metadata
  */
 export interface IModuleMetadata {
+  name?: string;
   routes?: Array<RouteRuleConfig>;
   modules?: Array<IModuleMetadata>;
   controllers: Array<IProvider|Function>;
